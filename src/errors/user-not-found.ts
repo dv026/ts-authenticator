a6ee22efaa2217f1daacae8a74ffc752ab881cbd
Dmitrii Vasilev <dimavas026@gmail.com>
2023-06-14 22:25:16 +0300

    test error message

--- src/errors/user-not-found.ts
+++ src/errors/user-not-found.ts
@@ -1,5 +1,10 @@
 export class UserNotFound extends Error {
+  errorCode: number
+  name: string
+
   constructor() {
     super("User Not Found")
+    this.name = "name1"
+    this.errorCode = 303
   }
 }
